42d858043bd0069bd16358b6e9119fb13eaf86b3
Chesa <chesakeselekkebo@gmail.com>
2022-06-14 06:54:19 +0700

    Added font Poppins

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,6 +24,13 @@ module.exports = {
           danger: '#FA2C5A',
           warning: '#F9CC00',
           success: '#73CA5C'
+        },
+        neutral: {
+          1: '#FFFFFF',
+          2: '#D0D0D0',
+          3: '#8A8A8A',
+          4: '#3C3C3C',
+          5: '#151515',
         }
       }
     },
