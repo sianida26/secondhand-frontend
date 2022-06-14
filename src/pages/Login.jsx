adfd92b579c4fbd9e0493b5cdf1219f2678bbe75
Chesa <chesakeselekkebo@gmail.com>
2022-06-14 09:20:20 +0700

    Contoh

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { setToken } from '../redux/slices/authSlice'
 
 export default function Login() {
+
+  const dispatch = useDispatch();
+
+  const purakPurakLogin = () => {
+    const token = "purakPurakToken";
+    dispatch(setToken(token));
+  }
+
   return (
-    <div>Login</div>
+    <div>
+      <button onClick={ purakPurakLogin }>Login</button>
+    </div>
   )
 }
