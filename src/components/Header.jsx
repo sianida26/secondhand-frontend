9730b8c41eb241f7507da23e648eb01841189aaf
unknown <andrevikta20@gmail.com>
2022-06-21 20:36:56 +0800

    add ficamera

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ export default function Header(props) {
   return (
     <nav
       className="sticky top-0  w-full flex flex-wrap items-center justify-between py-4 bg-white text-black
-    shadow-none lg:shadow-md navbar navbar-expand-lg navbar-light px-5"
+    shadow-none lg:shadow-md navbar navbar-expand-lg navbar-light px-5 z-40"
     >
       <div className="container-fluid w-full flex flex-wrap items-center justify-between lg:justify-around">
         <div className="hidden lg:inline w-[5.88rem] h-8 bg-purple-5"></div>
