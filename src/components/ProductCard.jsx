f7c95e593cdbf0d74efe6f813bfa7579dd8ae7e0
sianida26 <chesakeselekkebo@gmail.com>
2022-07-21 20:25:55 +0700

    Fix line clamp

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -8,7 +8,7 @@ export default function ProductCard(props) {
 	const navigate = useNavigate();
 
 	return (
-		<Link to={ `/produk/${ props.id }` } className="shadow-low p-4 text-neutral-5 rounded-lg w-full">
+		<Link to={ `/produk/${ props.id }` } className="shadow-low p-4 text-neutral-5 rounded-lg w-full h-full flex flex-col">
 			{/* Product image */}
 			{props.isLoading ? (
 				<div className="w-full aspect-[7/5] animate-pulse bg-slate-700" />
@@ -27,11 +27,12 @@ export default function ProductCard(props) {
 					<div className="w-20 h-4 bg-slate-700 rounded-md"></div>
 				</div>
 			) : (
-				<div className="flex flex-col gap-1 mt-4">
-					<h1 className="">{props.name}</h1>
-					<p className="text-sm text-neutral-3">{props.category}</p>
-
-					<p>{formatRupiah(props.price)}</p>
+				<div className="flex flex-col gap-1 mt-4 justify-between flex-grow">
+					<h1 className="line-clamp-2">{props.name}</h1>
+					<div className="flex flex-col gap-1">
+						<p className="text-sm text-neutral-3">{props.category}</p>
+						<p>{formatRupiah(props.price)}</p>
+					</div>
 				</div>
 			)}
 		</Link>
